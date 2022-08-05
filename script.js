8aea9b85e4ab47649d5f69e7123a6c165ab1b96f
Renyellen <reny1934@gmail.com>
2022-08-05 09:55:39 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,26 @@
+ // Crie um array que receba 5 itens e exiba no console.
+let dados["amor","paixão","gostar","ficar","namorar"];
+console.log (dados);
+
+
+// Utilize um método para adicionar um nome ao inicio do array.
+dados.unshift("casar");
+console.log(dados);
+
+
+// Utilize um método para remover o último nome do array.
+dados.pop();
+console.log(dados);
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+dados.push("casar","divorcio");
+console.log(dados);
+
+// Utilize um método para remover o primeiro nome do array.
+dados.shift();
+console.log(dados);
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// let numbers = [7,5,6,3,8,9,2,1,4]
+let numbers =[7,5,6,3,8,9,2,1,4];
+console.log(numbers.sort);
